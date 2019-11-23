c43c947e745b60132ef1b6f3f9ee99da30bb8526
Sirius504 <igor00901@yandex.ru>
2019-11-24 01:17:37 +0300

    Grid Inspector behaves correctly

--- Assets/Scripts/View/Editor/GridInspector.cs
+++ Assets/Scripts/View/Editor/GridInspector.cs
@@ -1,39 +1,40 @@
-﻿using Tetris.View;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(GridRenderer))]
+[CustomEditor(typeof(GridInstaller))]
 public class GridInspector : Editor
 {
-    private GridRenderer gridRenderer;
+    private GridInstaller gridInstaller;
     private Transform transform;
-    private Vector2Int gridSize;
-    private float cellSize = 0.1f;
+    private const string CELL_SIZE_PREFS_KEY = "cell_size";
+    private float cellSize;
 
     private const float CELL_SIZE_MIN = 0.01f;
 
     private void OnEnable()
     {
-        gridRenderer = (GridRenderer)target;
-        transform = gridRenderer.transform;
-        gridSize = RecalculateGridSize();
+        gridInstaller = (GridInstaller)target;
+        transform = gridInstaller.transform;
+        gridInstaller.GridSize = RecalculateGridSize();
     }
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         float cellMaxSize = Mathf.Min(transform.lossyScale.x, transform.lossyScale.y);
-        Debug.Log(cellMaxSize);
+        EditorGUILayout.LabelField("Cell Size:");
+        cellSize = EditorPrefs.GetFloat(CELL_SIZE_PREFS_KEY, 0.1f);
         cellSize = EditorGUILayout.Slider(cellSize, CELL_SIZE_MIN, cellMaxSize);
-        gridSize = RecalculateGridSize();
-        EditorGUILayout.LabelField("Grid Size:", gridSize.ToString());
+        EditorPrefs.SetFloat(CELL_SIZE_PREFS_KEY, cellSize);
+        gridInstaller.GridSize = RecalculateGridSize();
+        EditorGUILayout.LabelField("Grid Size:", gridInstaller.GridSize.ToString());
     }   
 
     private Vector2Int RecalculateGridSize()
     {
         var scale = transform.lossyScale;
-        int gridSizeX = Mathf.CeilToInt(scale.x / cellSize);
-        int gridSizeY = Mathf.CeilToInt(scale.y / cellSize);
+        int gridSizeX = Mathf.FloorToInt(scale.x / cellSize);
+        int gridSizeY = Mathf.FloorToInt(scale.y / cellSize);
         return new Vector2Int(gridSizeX, gridSizeY);
     }
 }
