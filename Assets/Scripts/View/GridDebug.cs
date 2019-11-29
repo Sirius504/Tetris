8d2f60193e17d8f9832c2ca2addcb32adc61db23
Sirius504 <igor00901@yandex.ru>
2019-11-29 23:21:17 +0300

    Grid Debug now processes grid gaps

--- Assets/Scripts/View/GridDebug.cs
+++ Assets/Scripts/View/GridDebug.cs
@@ -11,7 +11,7 @@ namespace Tetris.View
         void Update()
         {
             Vector3 origin = grid.CellToWorld(new Vector3Int(0, 0, 0));
-            Vector3 topRight = grid.CellToWorld(new Vector3Int(size.x, size.y, 0));
+            Vector3 topRight = grid.CellToWorld(new Vector3Int(size.x, size.y, 0)) - grid.cellGap;
             Vector3 downRight = new Vector3(topRight.x, origin.y, 0f);
             Vector3 topLeft = new Vector3(origin.x, topRight.y, 0f);
             Debug.DrawLine(origin, topRight, Color.white);
