37c69721d2428ac2d1cf7dc5dbb146cddb0a0df9
Sirius504 <igor00901@yandex.ru>
2019-11-29 23:16:28 +0300

    Arguments check on cell deletion

--- Assets/Scripts/Model/GameGrid.cs
+++ Assets/Scripts/Model/GameGrid.cs
@@ -19,9 +19,7 @@ namespace Tetris.Model
 
         public void CreateCell(Vector2Int position, CellColorsEnum color)
         {
-            if ((position.x < 0 || position.x >= Cells.Length)
-                && (position.y < 0 || position.y >= Cells.Length))
-                throw new ArgumentOutOfRangeException("Passed coordinates are out of grid range.");
+            CheckIfArgumentsFallWithinSize(position);
 
             if (Cells[position.x, position.y] != null)
                 throw new ArgumentException("Existing cell found at passed coordinates.");
@@ -29,8 +27,15 @@ namespace Tetris.Model
             Cells[position.x, position.y] = new Cell(color);
         }
 
+        private void CheckIfArgumentsFallWithinSize(Vector2Int argument)
+        {
+            if ((argument.x < 0 || argument.x >= Cells.Length)
+                && (argument.y < 0 || argument.y >= Cells.Length))
+            throw new ArgumentOutOfRangeException("Passed coordinates are out of grid range.");
+        }
+
         private Cell[,] GenerateEmptyGrid(Vector2Int size)
-        {            
+        {
             var colorValues = (CellColorsEnum[])Enum.GetValues(typeof(CellColorsEnum));
             Cell[,] result = new Cell[size.x, size.y];
             return result;
@@ -38,6 +43,8 @@ namespace Tetris.Model
 
         internal void DeleteCell(Vector2Int position)
         {
+            CheckIfArgumentsFallWithinSize(position);
+
             if (Cells[position.x, position.y] == null)
                 return;
             Cells[position.x, position.y] = null;
